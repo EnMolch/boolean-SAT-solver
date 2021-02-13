0b37e30869da6fe99880e0de9d0935e8d65a836e
EnMolch <heckenberger.t-it19@it.dhbw-ravensburg.de>
2021-02-13 15:22:27 +0100

    fixed the string-processing compiler stuff, using this . something aint the smartest, as it is a pointer ...

--- string_processing.cpp
+++ string_processing.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include "string_processing.h"
 //implement the methods of the string-class
 string_processing::string::string(char* address) : first_char(address){}
@@ -6,11 +7,10 @@ int string_processing::string::strlen()
 {
 	// iterate through the characters, counting up till a zero Byte
 	int len = 0;
-	int iterator = this.first_char;
-	while (iterator != NULL)
+	char* iterator = this->first_char;
+	while (iterator[len] != 0)
 	{
 		len++;
-		iterator = iterator[1];
 	}
 	return len;
 }
@@ -18,13 +18,13 @@ int string_processing::string::strlen()
 int string_processing::string::stringcompare(string &comp_str)
 {
 	// compare current string with comp_str and return 1 if they are equal, else return 0
-	int max_len = this.strlen()
+	int max_len = this->strlen();
 	if (max_len != comp_str.strlen()) return 0; //check for equal length
 	else
 	{
 		for (int i = 0; i<max_len;i++)
 		{
-			if(this.first_char[i] != comp_str.first_char[i])
+			if(this->first_char[i] != comp_str.first_char[i])
 				return 0;	
 		}	
 		return 1; //only if they have equal length and are equal in all spots, return true
